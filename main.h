9528ac419fc78ef4bd7ebe22df94e87425e332fd
ivy-515 <ivy.am515@gmail.com>
2023-04-25 18:30:18 -0700

    header file

---
+++ main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct format - structure of function call
+ * @fo: format (c, s, i, d)
+ * @print: function to call
+ */
+
+typedef struct format
+{
+	char fo;
+	int (*print)(va_list *);
+} format_t;
+
+int _printf(const char *format, ...);
+int (*conversion(char c))(va_list *);
+int get_char(va_list *character);
+int get_string(va_list *string);
+int get_int(va_list *arg);
+
+int _putchar(char c);
+int str_len(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+#endif /* MAIN_H */
